a3c7846568e9c0645158acf28ee6aea455cb615c
Sahibuddin Ahmed <sahibuddin@gmail.com>
2021-09-21 22:42:24 +0530

    Initial Commit

---
+++ Chief/Chief.h
@@ -0,0 +1,18 @@
+//
+//  Chief.h
+//  Chief
+//
+//  Created by Sahibuddin Ahmed on 21/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Chief.
+FOUNDATION_EXPORT double ChiefVersionNumber;
+
+//! Project version string for Chief.
+FOUNDATION_EXPORT const unsigned char ChiefVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chief/PublicHeader.h>
+
+
